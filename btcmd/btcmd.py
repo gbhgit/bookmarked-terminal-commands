a0c3845a34164f01e1036128c0339d905267d49a
Wellington Mendes <wellmend0@gmail.com>
2022-07-07 11:07:13 -0300

    [pip] add __main__.py

--- btcmd/btcmd.py
+++ btcmd/btcmd.py
@@ -138,12 +138,3 @@ class BookmarkedTerminalCommands:
         """
         )
 
-
-def main():
-    favorite_commands_manager = BookmarkedTerminalCommands()
-    options = favorite_commands_manager.get_arguments()
-    favorite_commands_manager.run(options)
-
-
-if __name__ == "__main__":
-    main()
